6ecf9b2b736bed027b60c20b7e887363e0e58b9c
Connor Frendt <cjacksonfx@yahoo.com>
2018-12-10 17:27:24 -0800

    wired up app.vue

--- app/src/components/App.vue
+++ app/src/components/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <p>I am a paragraph tag</p>
   </div>
 </template>
 
--- app/src/main.js
+++ app/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import App from './App.vue';
+import App from './components/App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
